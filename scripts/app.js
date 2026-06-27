708e2e8fd97ec409f39a8052d4d8583e9fede10c
Nico <Nico.Natividad@sdgku.edu>
2026-06-27 09:46:06 -0800

    Task manager

---
+++ scripts/app.js
@@ -0,0 +1,68 @@
+
+const API="https://106api-b0bnggbsgnezbzcz.westus3-01.azurewebsites.net/api/tasks"
+
+
+
+function saveTask(){
+
+    const title = $("#txtTitle").val();
+    const desc = $("#txtDescription").val();
+    const color = $("#selColor").val();
+    const status = $("#selStatus").val();
+    const date = $("#selDate").val();
+    const budget = $("#numBudget").val();
+
+
+    const taskToSave = new Task(title,desc,color,status,date,budget);
+    console.log(taskToSave);
+
+    displayTask(taskToSave);
+
+}
+
+function loadTask(){
+    $.ajax({
+
+        type: "GET",
+        url: API,
+        success: function(data){
+            console.log(data);
+        },
+        error: function(err){
+            console.log(err)
+        }
+    });
+}
+
+function displayTask(task){
+    let display = `
+        <div class="display-card" style="border-color: ${task.color};">
+    <div class="display-content">
+        <h1 class="task-title">${task.title}</h1>
+        
+        <div class="task-desc">
+            <p>${task.desc}</p>
+        </div>
+        
+        <div class="task-meta">
+            <span class="status-badge">${task.status}</span>
+            <div class="date-budget">
+                <span class="date">${task.date}</span>
+                <span class="budget">${task.budget}</span>
+            </div>
+        </div>
+    </div>
+</div>
+    `;
+    $(".list").append(display);
+}
+function init(){
+    console. log("Hello world!")
+    $("#btnSave").click(saveTask);
+    
+
+}
+
+window.onload = init;
+
+
